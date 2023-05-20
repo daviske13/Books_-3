67c20813f181e33baea9ca592c95d40e3d1dbb34
KualaT <daviske13@gmail.com>
2023-05-20 14:14:05 -0500

    add

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cluster0');
 
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        },
+);
 
 module.exports = mongoose.connection;
-
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cluster0');
